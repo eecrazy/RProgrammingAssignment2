b1202478fc34fbd7e61a8e75491d5c0ba9c6bb89
lzy <elizhongyang@163.com>
2014-12-12 18:35:17 +0800

    first

--- cachematrix.R
+++ cachematrix.R
@@ -7,16 +7,31 @@
 # ，那么cachesolve将检索缓存中的逆矩阵。计算正方形矩阵的逆矩阵可以在R中通过solve函数来完成。例如，如果X是一个正方形的
 # 可逆矩阵，那么solve(X)会返回其逆矩阵。
 # 对于此作业，假设提供的矩阵始终可逆。
-
-
-
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
-
+# 第一个函数makeCacheMatrix创建了一个特殊的“矩阵”，实际上它是包含具有以下用途的函数的列表
+# 1.	设置矩阵值
+# 2.	获取矩阵值
+# 3.	计算逆矩阵
+# 4.	获取逆矩阵
 makeCacheMatrix <- function(x = matrix()) {
+		m <- NULL
+        set_matrix <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get_matrix <- function() x
 
+        set_inverse <- function(inverse) m <<- inverse
+        
+        get_inverse <- function() m
+        
+        list(set_matrix = set_matrix,
+        	 get_matrix = get_matrix,
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 
 	solve(X)
 
@@ -27,6 +42,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$get_inverse()
+        if(!is.null(m)) {
+                message("getting cached matrix data")
+                return(m)
+        }
+        data <- x$get_matrix()
+        m <- solve(data, ...)
+        x$set_inverse(m)
+        m
 }
 
 
